a86367e67a8b1b62dea237f189928570af0ea3f9
mnqobivezi-cmyk <mnqobi.vezi@gmail.com>
2026-04-06 12:45:13 +0200

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(
+  'https://ftlivzbxotabkfbxmgnk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0bGl2emJ4b3RhYmtmYnhtZ25rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NjY5NDYsImV4cCI6MjA5MTA0Mjk0Nn0.8RhQh1RusJrmy13DMWqRPXWV9h9vsFFpiZc9W8XpnV4'
+)
